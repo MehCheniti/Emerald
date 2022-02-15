ede6492f7c6d85a5dde3cb656afef0b59381c509
MehCheniti <mehdiwx91@hotmail.com>
2022-02-15 23:34:01 +0100

    Did my first Emerald program, recreating a cyclic barrier previously done in Java!

---
+++ IN5570/barrier.m
@@ -0,0 +1,38 @@
+const barrier <- monitor object barrierObject
+
+  const maxProcesses : Integer <- 4
+  var currentlyWaitingProcesses : Integer <- 0
+  const c : Condition <- Condition.create
+
+  export operation enter
+    stdout.putstring["A process has started. \n"]
+
+    if currentlyWaitingProcesses == maxProcesses - 1 then
+      loop
+        exit when currentlyWaitingProcesses == 0
+        signal c
+        currentlyWaitingProcesses <- currentlyWaitingProcesses - 1
+      end loop
+    else
+      currentlyWaitingProcesses <- currentlyWaitingProcesses + 1
+      stdout.putstring["There is/ are currently " || currentlyWaitingProcesses.asstring || " process(es) waiting. \n"]
+      wait c
+    end if
+
+  end enter
+end barrierObject
+
+% main program
+const main <- object main
+    initially
+      for i : Integer <- 1 while i < 5 by i <- i + 1
+        const aProcess <- object aProcess
+          const number <- i
+          process
+            barrier.enter
+            stdout.putstring["Process number " || number.asstring || " has passed the barrier. \n"]
+          end process
+        end aProcess
+      end for
+    end initially
+end main
